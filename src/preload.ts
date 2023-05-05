9391515b04e1a56a6c6b36376785ad32ed537ee2
Stiliyan Stoyanov <stiliyan_1993@abv.bg>
2023-05-05 22:26:16 +0300

    remove unused code

--- src/preload.ts
+++ src/preload.ts
@@ -1,8 +1,3 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 // https://www.electronjs.org/docs/latest/tutorial/context-isolation
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('stopwatchAPI', {
-    start: () => ipcRenderer.invoke('start')
-});
