ff3e6f354282a8d570511e570d2cfb31b227f589
Stiliyan Stoyanov <stiliyan_1993@abv.bg>
2023-05-10 21:06:01 +0300

    Added renderer listeners and logic

--- src/util/stopwatch.ts
+++ src/util/stopwatch.ts
@@ -14,7 +14,7 @@ export function stopwatch(): stopwatchInterface {
     let intervalId: NodeJS.Timer;
     let paused = true;
     let [hours, minutes, seconds, milliseconds] = [0, 0, 0, 0];
-
+    
     const startTimer = (onIntervalTick: onIntervalTickCallback) => {
         clearInterval(intervalId);
         paused = false;
@@ -43,4 +43,55 @@ export function stopwatch(): stopwatchInterface {
         pauseTimer,
         resetTimer,
     }
+}
+
+export function stopwatchSetup() {
+    const startPauseButton = document.getElementById('start-pause-button');
+    const playPause = document.getElementById('play-pause')
+    const resetButton = document.getElementById('reset-button') as HTMLButtonElement;
+    const hoursElement = document.getElementById('hours');
+    const minutesElement = document.getElementById('minutes');
+    const secondsElement = document.getElementById('seconds');
+    const millisecondsElement = document.getElementById('milliseconds');
+    let paused = true;
+    let isRunning = false;
+    const { startTimer, pauseTimer, resetTimer } = stopwatch();
+
+    startPauseButton.addEventListener('click', () => {
+        startPauseButton.classList.toggle("active");
+        if (!paused) {
+            console.log('timer is paused');
+            pauseTimer();
+            paused = true;
+            return;
+        }
+        if (!isRunning) {
+            resetButton.classList.toggle('sb-active');
+            resetButton.disabled = false;
+        }
+
+        console.log('timer is running');
+        paused = false;
+        isRunning = true
+        startTimer(([hours, minutes, seconds, milliseconds]) => {
+            hoursElement.textContent = hours;
+            minutesElement.textContent = minutes;
+            secondsElement.textContent = seconds;
+            millisecondsElement.textContent = milliseconds;
+        })
+    })
+
+    resetButton.addEventListener('click', () => {  
+        console.log('timer is reset');
+        if (!paused) startPauseButton.classList.toggle("active");
+        resetButton.classList.toggle('sb-active');
+        resetButton.disabled = true;
+        paused = true;
+        isRunning = false;
+        hoursElement.textContent = '00'
+        minutesElement.textContent = '00'
+        secondsElement.textContent = '00'
+        millisecondsElement.textContent = '00'
+        resetTimer();
+    })
 }
