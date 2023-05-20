78fbc03fc3b40e45a99cbbbf06cbb3c809bc01fb
Stiliyan Stoyanov <stiliyan_1993@abv.bg>
2023-05-20 21:45:51 +0300

    Fix timing

--- src/util/stopwatchUtil.ts
+++ src/util/stopwatchUtil.ts
@@ -1,10 +1,10 @@
-export type timesStringArray = [hours: string, minutes: string, seconds: string, milliseconds: string];
+export type TimeStringArray = [hours: string, minutes: string, seconds: string, milliseconds: string];
 
 function getCustomTimeArray(time: number): [hours: number, minutes: number, seconds: number, milliseconds: number] {
     return [ 
-        Math.floor(time / 6000),
-        Math.floor(time / 600), 
-        Math.floor(time / 10), 
+        Math.floor(time / 36000),
+        Math.floor(time / 600) % 60, 
+        Math.floor(time / 10) % 60, 
         time % 10 * 10
     ]
 }
@@ -12,8 +12,8 @@ function addLeadingZero(time: number) {
     return time <= 9 ? `0${time}` : time.toString();
 }
 
-export function formatTime(time: number): timesStringArray {
-    return getCustomTimeArray(time).map(time => addLeadingZero(time)) as timesStringArray
+export function formatTime(time: number): TimeStringArray {
+    return getCustomTimeArray(time).map(time => addLeadingZero(time)) as TimeStringArray
 }
 
 /**
