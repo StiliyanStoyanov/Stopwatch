e7a3cc91a54f89666bdc46cd0d3cc3bbf44520c7
Stiliyan Stoyanov <stiliyan_1993@abv.bg>
2023-05-31 21:13:32 +0300

    Add and update stopwatch view utility functions

--- src/util/stopwatchViewUtil.ts
+++ src/util/stopwatchViewUtil.ts
@@ -1,38 +1,53 @@
-export function createTimeTrackerElement(laps: number, lapTime: string, totalTime: string) {
+export function createLapHeader() {
     const template = document.createElement('template');
     const templateString = `
-    <div id="times-tracker">
-        <section>
-            <h3>Laps</h3>
-            <ul id="laps">
-                <li>
-                    <span>${laps}</span>
-                    <span id="fastest">&nbspFastest</span>
-                    <span id="slowest">&nbspSlowest</span>
-                </li>
-            </ul>
-        </section>
-        <section>
-            <h3>Time</h3>
-            <ul id="time">
-                <li>
-                    <span>${lapTime}</span>
-                </li>
-            </ul>
-        </section>
-        <section>
-            <h3>Total</h3>
-            <ul id="total">
-                <li>
-                    <span> ${totalTime}</span>
-                </li>
-            </ul>
-        </section>
+    <div class="lap-row laps-inner-container">
+        <h3>Laps</h3>
+        <h3>Time</h3>
+        <h3>Total</h3>
     </div>
     `
     templateString.trim();
     template.innerHTML = templateString
     return template.content;
+}
+export function createLapTracker(lapCount: number, lapTime: string, totalTime: string) {
+    const template = document.createElement('template');
+    const templateString = `
+    <div id="laps-tracker" class="laps-inner-container">
+        <div class="lap-row">
+            <p>${lapCount}<span id="fastest">&nbsp;Fastest</span><span id="slowest">&nbsp;Slowest</span></p>
+            <p>${lapTime}</p>
+            <p>${totalTime}</p>
+        </div>
+    </div>
+    `
+    templateString.trim();
+    template.innerHTML = templateString
+    return template.content;
+}
+
+export function createLapRowElement(lapCount:number, lapTime: string, totalTime: string, isFastest: boolean, isSlowest: boolean) {
+    const lapRowElement = document.createElement('div');
+    const lapCountElement = document.createElement('p');
+    const lapTimeElement = document.createElement('p');
+    const totalTimeElement = document.createElement('p');
+    lapRowElement.className = "lap-row"
+    lapCountElement.textContent = lapCount.toString();
+    lapTimeElement.textContent = lapTime;
+    totalTimeElement.textContent = totalTime;
+
+    if (isFastest) {
+        lapCountElement.append(document.getElementById('fastest'));
+    }
+
+    if (isSlowest) {
+        lapCountElement.append(document.getElementById('slowest'));
+    }
+    lapRowElement.append(lapCountElement);
+    lapRowElement.append(lapTimeElement);
+    lapRowElement.append(totalTimeElement);
+    return lapRowElement;
 }
 
 export function createElementWithText(tagName: keyof HTMLElementTagNameMap, textContent: number | string) {
