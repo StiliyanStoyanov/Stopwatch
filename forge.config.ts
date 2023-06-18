c90d2228cf73fd78838e6aa92cf1e406d7a7ea59
Stiliyan Stoyanov <stiliyan_1993@abv.bg>
2023-06-18 09:34:17 +0300

    update configs

--- forge.config.ts
+++ forge.config.ts
@@ -5,6 +5,7 @@ import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
+
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
 
@@ -21,7 +22,6 @@ const config: ForgeConfig = {
           owner: 'StiliyanStyoanov',
           name: 'stopwatch'
         },
-        draft: true
       }
     }
   ],
